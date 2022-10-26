59fd4c579944902f9d6864050f0597ff68e31137
marhau-dev <111030902+marhau-dev@users.noreply.github.com>
2022-10-25 20:57:10 -0500

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+echo Hi i Installer Spotify (no ads) 
+
+echo (The author is not responsible for any problems and does not support piracy in any example, so you install this hack at your own risk.)
+
+echo Press any key if you agree with these rules
+
+PAUSE
+
+call 1.bat
+
+pause 
+
+call 2.bat
