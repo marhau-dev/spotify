74f831bcf345c9c048f35c7e8ee9654d2c243110
marhau-dev <111030902+marhau-dev@users.noreply.github.com>
2024-09-27 09:45:58 -0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify""
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify""
 
 pause
 exit /b
